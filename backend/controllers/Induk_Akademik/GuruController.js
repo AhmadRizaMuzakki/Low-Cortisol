3d71755ec687806b0d33759329b08015ed0b8f4c
AhmadRizaMuzakki <rizaahmad934@gmail.com>
2026-04-05 01:11:27 +0700

    crud data induk akademik selesai

--- backend/controllers/Induk_Akademik/GuruController.js
+++ backend/controllers/Induk_Akademik/GuruController.js
@@ -2,16 +2,46 @@ const GuruModel = require('../../models/induk_akademik/GuruModel');
 
 class GuruController {
     index(req, res) {
-        res.send('Hello World guru');
+        GuruModel.getAllGuru((err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Guru berhasil diambil', data: results });
+            }
+        });
     }
     store(req, res) {
-        res.send('Hello World');
+        const { nip, nama_guru,jenis_kelamin, no_hp, user_id } = req.body;
+        const guru = { nip, nama_guru, jenis_kelamin, no_hp, user_id };
+        GuruModel.createGuru(guru, (err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Guru berhasil ditambahkan', data: results });
+            }
+        });
     }
     update(req, res) {
-        res.send('Hello World');
+        const { id } = req.params;
+        const { nip, nama_guru, jenis_kelamin, no_hp, user_id } = req.body;
+        const guru = { id, nip, nama_guru, jenis_kelamin, no_hp, user_id };
+        GuruModel.updateGuru(id, guru, (err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Guru berhasil diubah', data: results });
+            }
+        });
     }
     destroy(req, res) {
-        res.send('Hello World');
+        const { id } = req.params;
+        GuruModel.deleteGuru(id, (err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Guru berhasil dihapus', data: results });
+            }
+        });
     }
 }
 module.exports = new GuruController();
--- backend/controllers/Induk_Akademik/KelasController.js
+++ backend/controllers/Induk_Akademik/KelasController.js
@@ -2,16 +2,46 @@ const KelasModal = require('../../models/induk_akademik/KelasModal');
 
 class KelasController {
     index(req, res) {
-        res.send('Hello World kelas');
+        KelasModal.getAllKelas((err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Kelas berhasil diambil', data: results });
+            }
+        });
     }
     store(req, res) {
-        res.send('Hello World');
+        const { nama_kelas, tahun_ajaran, id_guru_kelas } = req.body;
+        const kelas = { nama_kelas, tahun_ajaran, id_guru_kelas };
+        KelasModal.createKelas(kelas, (err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Kelas berhasil ditambahkan', data: results });
+            }
+        });
     }
     update(req, res) {
-        res.send('Hello World');
+        const { id } = req.params;
+        const { nama_kelas, tahun_ajaran, id_guru_kelas } = req.body;
+        const kelas = { id, nama_kelas, tahun_ajaran, id_guru_kelas };
+        KelasModal.updateKelas(id, kelas, (err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Kelas berhasil diubah', data: results });
+            }
+        });
     }
     destroy(req, res) {
-        res.send('Hello World');
+        const { id } = req.params;
+        KelasModal.deleteKelas(id, (err, results) => {
+            if(err){
+                res.status(500).json({ error: err.message });
+            }else{
+                res.status(200).json({ message: 'Kelas berhasil dihapus', data: results });
+            }
+        });
     }
 }
 module.exports = new KelasController();
