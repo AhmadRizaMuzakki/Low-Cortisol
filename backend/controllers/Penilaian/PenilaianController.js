436da8ad5cd9ca1fd457daa725adb5023761021e
AryaNuryawan <aryanuryawann@gmail.com>
2026-04-09 00:31:34 +0700

    Membuat CRUD Penilaian

--- backend/controllers/Penilaian/PenilaianController.js
+++ backend/controllers/Penilaian/PenilaianController.js
@@ -1,15 +1,45 @@
+const PenilaianModels = require('../../models/Penilaian/PenilaianModel');
+
 class PenilaianController {
     index(req, res) {
-        res.send('Hello World');
+        PenilaianModels.getAllPenilaian((err, results) => {
+            if(err) {
+                res.status(500).json({ error: err.message });
+            } else {
+                res.json(results);
+            }
+        });
     }
     store(req, res) {
-        res.send('Hello World');
+        const penilaian = req.body;
+        PenilaianModels.createPenilaian(penilaian, (err, results) => {
+            if(err) {
+                res.status(500).json({ error: err.message });
+            } else {
+                res.status(201).json(results);
+            }
+        });
     }
     update(req, res) {
-        res.send('Hello World');
+        const id = req.params.id;
+        const penilaian = req.body;
+        PenilaianModels.updatePenilaian(id, penilaian, (err, results) => {
+            if(err) {
+                res.status(500).json({ error: err.message });
+            } else {
+                res.json(results);
+            }
+        });
     }
     destroy(req, res) {
-        res.send('Hello World');
+        const id = req.params.id;
+        PenilaianModels.deletePenilaian(id, (err, results) => {
+            if(err) {
+                res.status(500).json({ error: err.message });
+            } else {
+                res.json(results);
+            }
+        });
     }
 }
 
