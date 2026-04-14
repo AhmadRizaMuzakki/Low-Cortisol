8ed135b12c5b8ff63846bf43fe78790b8d9238ae
AhmadRizaMuzakki <rizaahmad934@gmail.com>
2026-04-14 22:26:58 +0700

    membuat validasi dan error induk_akademik

--- backend/controllers/Induk_Akademik/KelasController.js
+++ backend/controllers/Induk_Akademik/KelasController.js
@@ -1,45 +1,62 @@
 const KelasModal = require('../../models/induk_akademik/KelasModal');
+const validationId = require('../../utils/ValidationController/ValidationId');
+const validationKelas = require('../../utils/ValidationController/ValidationKelas');
+const AppError = require('../../utils/AppError');
 
 class KelasController {
     index(req, res) {
         KelasModal.getAllKelas((err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Kelas berhasil diambil', data: results });
+                return AppError(res, results, 200, 'Kelas berhasil diambil');
             }
         });
     }
     store(req, res) {
-        const { nama_kelas, tahun_ajaran, id_guru_kelas } = req.body;
-        const kelas = { nama_kelas, tahun_ajaran, id_guru_kelas };
-        KelasModal.createKelas(kelas, (err, results) => {
+        const data = req.body;
+        const error = validationKelas(data);
+        if (error) {
+            return AppError(res, error, 400, error.error);
+        }
+        KelasModal.createKelas(data, (err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Kelas berhasil ditambahkan', data: results });
+                return AppError(res, results, 201, 'Kelas berhasil ditambahkan');
             }
         });
     }
     update(req, res) {
         const { id } = req.params;
-        const { nama_kelas, tahun_ajaran, id_guru_kelas } = req.body;
-        const kelas = { id, nama_kelas, tahun_ajaran, id_guru_kelas };
-        KelasModal.updateKelas(id, kelas, (err, results) => {
+        const AppError = validationId(id);
+        if (AppError) {
+            return AppError(res, AppError, 400, AppError.error);
+        }
+        const data= req.body;
+        const bodyError = validationKelas(data);
+        if (bodyError) {
+            return AppError(res, bodyError, 400, bodyError.error);
+        }
+        KelasModal.updateKelas(id, data, (err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Kelas berhasil diubah', data: results });
+                return AppError(res, results, 200, 'Kelas berhasil diubah');
             }
         });
     }
     destroy(req, res) {
         const { id } = req.params;
+        const AppError = validationId(id);
+        if (AppError) {
+            return AppError(res, AppError, 400, AppError.error);
+        }
         KelasModal.deleteKelas(id, (err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Kelas berhasil dihapus', data: results });
+                return AppError(res, results, 200, 'Kelas berhasil dihapus');
             }
         });
     }
--- backend/controllers/Induk_Akademik/SiswaController.js
+++ backend/controllers/Induk_Akademik/SiswaController.js
@@ -1,46 +1,62 @@
 const SiswaModel = require('../../models/induk_akademik/SiswaModel');
-
+const validationId = require('../../utils/ValidationController/ValidationId');
+const validationSiswa = require('../../utils/ValidationController/ValidationSiswa');
+const AppError = require('../../utils/AppError');
 
 class SiswaController {
     index(req, res) {
         SiswaModel.getAllSiswa((err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Siswa berhasil diambil', data: results });
+                return AppError(res, results, 200, 'Siswa berhasil diambil');
             }
         });
     }
     store(req, res) {
-        const { nisn, nama_siswa, tanggal_lahir, jenis_kelamin, alamat, id_kelas, user_id } = req.body;
-        const siswa = { nisn, nama_siswa, tanggal_lahir, jenis_kelamin, alamat, id_kelas, user_id };
-        SiswaModel.createSiswa(siswa, (err, results) => {
+        const data = req.body;
+        const error = validationSiswa(data);
+        if (error) {
+            return AppError(res, error, 400, error.error);
+        }
+        SiswaModel.createSiswa(data, (err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Siswa berhasil ditambahkan', data: results });
+                return AppError(res, results, 201, 'Siswa berhasil ditambahkan');
             }
         });
     }
     update(req, res) {
         const { id } = req.params;
-        const { nisn, nama_siswa, tanggal_lahir, jenis_kelamin, alamat, id_kelas, user_id } = req.body;
-        const siswa = { id, nisn, nama_siswa, tanggal_lahir, jenis_kelamin, alamat, id_kelas, user_id };
-        SiswaModel.updateSiswa(id, siswa, (err, results) => {
+        const AppError = validationId(id);
+        if (AppError) {
+            return AppError(res, AppError, 400, AppError.error);
+        }
+        const data = req.body;
+        const bodyError = validationSiswa(data);
+        if (bodyError) {
+            return AppError(res, bodyError, 400, bodyError.error);
+        }
+        SiswaModel.updateSiswa(id, data, (err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Siswa berhasil diubah', data: results });
+                return AppError(res, results, 200, 'Siswa berhasil diubah');
             }
         });
     }
     destroy(req, res) {
         const { id } = req.params;
+        const AppError = validationId(id);
+        if (AppError) {
+            return AppError(res, AppError, 400, AppError.error);
+        }
         SiswaModel.deleteSiswa(id, (err, results) => {
             if(err){
-                res.status(500).json({ error: err.message });
+                return AppError(res, err, 500, err.message);
             }else{
-                res.status(200).json({ message: 'Siswa berhasil dihapus', data: results });
+                return AppError(res, results, 200, 'Siswa berhasil dihapus');
             }
         });
     }
