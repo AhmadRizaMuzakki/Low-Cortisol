3fe0ebe9c1002c58fca21ab9e72b5a4c3805ff8a
AhmadRizaMuzakki <rizaahmad934@gmail.com>
2026-04-21 21:57:27 +0700

    membuat fitur login dan register

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const router = require('./routes/api');
 const db = require('./config/database');
@@ -22,7 +23,9 @@ app.get('/test-koneksi', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
